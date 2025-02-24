a8af13d9ba15b9f99b8a43b32058b95d2a3784e1
kansihin <kansihin@gmail.com>
2025-02-24 16:43:56 +0900

    add new

--- document/learndockerfile/add_br_to_lines.py
+++ document/learndockerfile/add_br_to_lines.py
@@ -23,6 +23,6 @@ def add_br_to_lines(input_filename, output_filename):
                 outfile.write(line_content + '<br>\n')
 
 if __name__ == '__main__':
-    input_file = 'Dockerfile_VS_DockerCompose.html'    # 输入 HTML 文件路径
-    output_file = 'Dockerfile_VS_DockerCompose2.html'  # 输出 HTML 文件路径
+    input_file = '其它制作镜像的方式.html'    # 输入 HTML 文件路径
+    output_file = '其它制作镜像的方式2.html'  # 输出 HTML 文件路径
     add_br_to_lines(input_file, output_file)
